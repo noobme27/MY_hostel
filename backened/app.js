4492dbd53ccd14ddd698081ffd642e1be01dc843
noobapoo99 <apoorvtripathi99@gmail.com>
2024-10-10 09:33:04 +0530

    register

--- backened/app.js
+++ backened/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors"; // Add this
 import layoutRoute from "./routes/hosteLayout.route.js";
+import authRoute from "./routes/auth.route.js";
 const app = express();
 
 app.use(cors()); // Enable CORS
 
 app.use("/api/room-layout", layoutRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800!");
