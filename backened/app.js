c49b482445addc209022a51a950e6bb579115732
noobapoo99 <apoorvtripathi99@gmail.com>
2024-10-15 18:36:39 +0530

    login and signup page now connected to backened , also all the neccesary packages are being uploaded in readme

--- backened/app.js
+++ backened/app.js
@@ -1,10 +1,10 @@
 import express from "express";
-import cors from "cors"; // Add this
+import cors from "cors";
 import layoutRoute from "./routes/hosteLayout.route.js";
 import authRoute from "./routes/auth.route.js";
 const app = express();
 
-app.use(cors()); // Enable CORS
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 app.use("/api/room-layout", layoutRoute);
