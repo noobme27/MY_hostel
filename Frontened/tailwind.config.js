2fa4419bff940be1732139e7125cd183c56f9474
Apoorv Nath Tripathi <apoorvtripathi99@gmail.com>
2024-12-11 21:18:23 +0530

    improved color code

--- Frontened/tailwind.config.js
+++ Frontened/tailwind.config.js
@@ -1,5 +1,6 @@
 import daisyui from "daisyui";
-const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from "@material-tailwind/react/utils/withMT";
+
 /** @type {import('tailwindcss').Config} */
 export default withMT({
   content: [
